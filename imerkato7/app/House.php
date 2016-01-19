b536ca09da4d4a5398ebbe194802120d13cf3e44
Tekalegn Fisseha <techhand@gmail.com>
2016-01-19 02:06:42 -0500

    article and house modified

--- imerkato7/app/House.php
+++ imerkato7/app/House.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class House extends Model
 {
-    //
+    //These fileds are allowed for mass assignmet
+    protected $fillable = [
+
+    	'user_username',
+    	'user_email',
+    	'country',
+    	'state',
+    	'city',
+    	'category',
+    	'type',
+    	'title',
+    	'price',
+    	'currency_type',
+    	'detial',
+    	'contact_imerkato',
+    	'contact_phone',
+    	'image_path',
+    	'thumb_path',
+    ];
 }
