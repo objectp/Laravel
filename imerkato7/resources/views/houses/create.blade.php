77993dfb5f2eb06ef87a7b40ac48c8566ceb726f
Tekalegn Fisseha <techhand@gmail.com>
2016-02-02 11:29:30 -0500

    house form and create files modified

--- imerkato7/resources/views/houses/create.blade.php
+++ imerkato7/resources/views/houses/create.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 
 @section('content')
@@ -6,31 +5,125 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Create House</div>
-
+                <div class="panel-heading">Add House</div>
                 <div class="panel-body">
-                    
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/houses') }}">
                         {!! csrf_field() !!}
 
-                              <div class="panel-heading">Insert new house</div>
+                        <div class="form-group{{ $errors->has('category') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Category</label>
 
-                        {!! Form::open(['url' => 'houses']) !!}
-            
-                              @include ('houses.form', ['submitButtonText' => 'Add House'])
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="category" value="{{ old('category') }}">
 
-                        {!! Form::close() !!}
+                                @if ($errors->has('category'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('category') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
+                        <div class="form-group{{ $errors->has('type') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Type</label>
 
-                        @include('errors.list')
-            
-                      </div>
-                 
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="type" value="{{ old('type') }}">
+
+                                @if ($errors->has('type'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('type') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Title</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="title">
+
+                                @if ($errors->has('title'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('title') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('pirce') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Pirce</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="pirce">
+
+                                @if ($errors->has('pirce'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('pirce') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('currency_type') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Currency Type</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="currency_type">
+
+                                @if ($errors->has('currency_type'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('currency_type') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('detail') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Detail</label>
+
+                            <div class="col-md-6">
+                                <input type="textarea" class="form-control" name="detail">
+
+                                @if ($errors->has('detail'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('detail') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    <i></i>Add House
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+
+
 
 
 
--- imerkato7/resources/views/houses/form.blade.php
+++ imerkato7/resources/views/houses/form.blade.php
@@ -1,7 +1,16 @@
-        <div class="form-group">
+
+
+         <div class="form-group{{ $errors->has('category') ? ' has-error' : '' }}">
 			{!! Form::label('category', 'Category:') !!}
 			{!! Form::text('category') !!}
 		<div>
+		
+		<div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+	        {{ Form::label('title', 'Title') }}
+	        {{ Form::text('title', null, ['class' => 'form-control']) }}
+	        {{ $errors->first('title', '<p class="help-block">:message</p>') }}
+        </div>
+
 		<div class="form-group">
 			{!! Form::label('type', 'Type:') !!}
 			{!! Form::text('type') !!}
@@ -29,8 +38,9 @@
 		<div class="form-group">
 			{!! Form::label('contact_phone', 'Contact me by my phone:') !!}
 			{!! Form::text('contact_phone') !!}
-		<div>
+		<div>  
 	
 		<div class="form-group">
 			{!! Form::submit($submitButtonText, ['class'=>'btn-primary form-control']) !!}
 		<div>
+		
