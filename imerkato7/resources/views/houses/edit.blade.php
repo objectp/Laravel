4cff73beb18f32b68f9eb7507be4b493df541ed7
Tekalegn Fisseha <techhand@gmail.com>
2016-02-03 11:09:29 -0500

    house edit file modified

--- imerkato7/resources/views/houses/edit.blade.php
+++ imerkato7/resources/views/houses/edit.blade.php
@@ -1,16 +1,124 @@
-@extends('masterHtml')
-
+@extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Edit House</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/houses') }}">
+                        {!! csrf_field() !!}
 
-<h1>Edit {!! $house->title !!}</h1>
+                        <div class="form-group{{ $errors->has('category') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Category</label>
 
-	{!! Form::model([$house, 'method' => 'PATCH', 'action' => ['HousesController@update', $house->id]]) !!}
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="category" value="{{ old('category') }}">
 
+                                @if ($errors->has('category'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('category') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
-		@include ('houses.form', ['submitButtonText' => 'Update House'])
+                        <div class="form-group{{ $errors->has('type') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Type</label>
 
-	{!! Form::close() !!}
-		
-	@include('errors.list')
-@stop
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="type" value="{{ old('type') }}">
+
+                                @if ($errors->has('type'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('type') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Title</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="title">
+
+                                @if ($errors->has('title'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('title') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('pirce') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Pirce</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="pirce">
+
+                                @if ($errors->has('pirce'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('pirce') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('currency_type') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Currency Type</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="currency_type">
+
+                                @if ($errors->has('currency_type'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('currency_type') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('detail') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Detail</label>
+
+                            <div class="col-md-6">
+                                <input type="textarea" class="form-control" name="detail">
+
+                                @if ($errors->has('detail'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('detail') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    <i></i>Edit House
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
